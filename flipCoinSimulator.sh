52f1a2f464c00caa40569d6d891120c56078c75e
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-12 11:20:13 +0530

    Usercase1 done

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,12 @@
 # welcome to flip coin simulator
+read -p "Enter the no of times to flip coin: " a
+for(( count=0;count<a;count++ ))
+do 
+   	random=$(( RANDOM%2 ))
+	if [ $random -eq 0 ]
+	then 
+	echo "Heads"
+	else
+	echo "Tails"
+	fi
+done 
