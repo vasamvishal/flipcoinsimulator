13ef3f0123241aa89d65452e59945a8ea3be531f
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-12 12:03:01 +0530

    usercase 2 done on branch

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,30 @@
 # welcome to flip coin simulator
 read -p "Enter the no of times to flip coin: " a
+declare -A dictionary
+tail=0;
+head=0;
 for(( count=0;count<a;count++ ))
 do 
    	random=$(( RANDOM%2 ))
 	if [ $random -eq 0 ]
 	then 
 	echo "Heads"
+        head=$(( $head+1 ))
 	else
 	echo "Tails"
+        tail=$(( $tail+1 ))
+        
 	fi
-done 
+done
+dictionary[head]=$head
+dictionary[tail]=$tail
+
+ echo ${dictionary[@]}
+
+result=$(( $head*100 ))
+headsresult=$(( $result/$a))
+result=$(( $tail*100 ))
+tailsresult1=$(( $result/$a))
+
+echo ${dictionary1[@]}
+
