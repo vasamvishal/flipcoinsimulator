8d9bf900a256052950e55d4ac145dda148407cb1
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-11-12 17:33:00 +0530

    usercase 4 completed

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,7 +9,90 @@ tailheaddouble=0;
 headtailhead=0;
 tailheadtail=0;
 
+headdouble=0;
+taildouble=0;
+headtail=0;
+tailhead=0;
+declare -A dictionary
+function flipcoin1()
+{
+tail=0;
+head=0;
+for(( count=0;count<a;count++ ))
+do 
+   	random=$(( RANDOM%2 ))
+	if [ $random -eq 0 ]
+	then 
+	echo "Heads"
+        head=$(( $head+1 ))
+	else
+	echo "Tails"
+        tail=$(( $tail+1 ))
+        
+	fi
+done
+result=$(( $head*100 ))
+headsresult=$(( $result/$a))
+result=$(( $tail*100 ))
+tailsresult1=$(( $result/$a))
+}
+flipcoin1 $a
+dictionary[head]=$head
+dictionary[tail]=$tail
+
+echo ${dictionary[@]}
+
+
 function flipcoin(){
+declare -A dictionary
+tail=1;
+head=0;
+for(( count=0;count<$1;count++ ))
+do
+   	random=$(( RANDOM%2 ))
+         random1=$(( RANDOM%2 ))
+
+	if [ $random -eq $random1 ]
+	then
+        if [ $random -eq $head ]
+        then 
+        	 echo "HeadsHeads"
+        	 headdouble=$(( $headdouble+1 ))
+	else
+		echo "TailsTails"
+        	taildouble=$(( $taildouble+1 ))
+        
+
+ 	fi
+	else
+        	if [ $random -eq $head ]
+                then
+		headtail=$(( $headtail+1 ))
+                else
+                tailhead=$(( $tailhead+1 ))
+	fi
+	fi
+done
+echo $headtail
+echo ${dictionary[@]}
+result=$(( $headtail*100 ))
+headtailresult=$(( $result/$a))
+result=$(( $tailhead*100 ))
+tailheadresult=$(( $result/$a))
+result=$(( $headdouble*100 ))
+headdoubleresult=$(( $result/$a))
+result=$(( $taildouble*100 ))
+taildoubleresult=$(( $result/$a))
+}
+
+ flipcoin $a
+dictionary[headtail]=$headtail
+dictionary[tailhead]=$tailhead
+dictionary[headdouble]=$headdouble
+dictionary[taildouble]=$taildouble
+
+
+function flipcoin2(){
 declare -A dictionary
 tail=1;
 head=0;
@@ -25,20 +108,20 @@ do
         	 echo "HeadsHeads"
         	 headtriplet=$(( $headtriplet+1 ))
 	else
+
 		echo "TailsTails"
         	tailtriplet=$(( $tailtriplet+1 ))
- 	fi
-	
-
+ 
+	fi
 	elif [ $random -eq $random1 ]
 		then
 	     if [ $random -eq $head  ]
-		then
+	then
 		headdoubletail=$(( $headdoubletail+1 ))
-                else
+               else
                 taildoublehead=$(( $taildoublehead+1 ))
         	fi
-        	
+
 	 elif (( $random != $random1 & $random1 ==$random2 ))
                 then
              if [ $random -eq $head ]
@@ -73,17 +156,28 @@ result=$(($headtaildouble*100 ))
 headtaildoubleresult=$(( $result/$a))
 result=$(( $tailheaddouble*100 ))
 tailheaddoubleresult=$(( $result/$a))
+
 result=$(( $headtailhead*100 ))
 headtailheadresult=$(( $result/$a))
 result=$(( $tailheadtail*100 ))
 tailheadtailresult=$(( $result/$a))
-
-
 }
 
- flipcoin $a 
+flipcoin2 $a 
+dictionary[headdoubletail]=$headdoubletail
+dictionary[taildoublehead]=$taildoublehead
+dictionary[headtriplet]=$headtriplet
+dictionary[tailtriplet]=$tailtriplet
+dictionary[headtaildouble]=$headtaildouble
+dictionary[tailheaddouble]=$tailheaddouble
+dictionary[headtailhead]=$headtailhead
+dictionary[tailheadtail]=$tailheadtail
 
- #echo ${dictionary[@]}
+result=$( printf "%s " ${!dictionary[@]} ${dictionary[@]} | sort -nr | awk {'print $1'}   )
+echo ${dictionary[result]}
+
+
+ echo ${dictionary[@]}
 
 #result=$(( $headtail*100 ))
 #headtailresult=$(( $result/$a))
@@ -92,7 +186,7 @@ tailheadtailresult=$(( $result/$a))
 #result=$(( $headdouble*100 ))
 #headdoubleresult=$(( $result/$a))
 
-#result=$(( $taildouble*100 ))
+#esult=$(( $taildouble*100 ))
 #taildoubleresult=$(( $result/$a))
 
 
